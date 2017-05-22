543f84e730e42adc720395b400e38af51a380960
ullpita <upierre-@student.42.fr>
2017-05-22 18:56:33 +0200

    Update rostring.c

--- lvl4/rostring.c
+++ lvl4/rostring.c
@@ -66,17 +66,67 @@ int		main(int ac, char **av)
 {
 	int i;
 
-	if (ac > 1)
+	i = 0;
+	if (ac < 2)
+		write(1, "\n", 1);
+	else
 	{
 		i = 1;
 		while (i < ac)
 		{
 			rostring(av[i]);
-			ft_putchar('\n');
 			i++;
+			write(1, "\n", 1);
 		}
 	}
-	else
-		ft_putchar('\n');
 	return (0);
+}
+
+
+int		rostring(char *str)
+{
+	int		i;
+	int		j;
+	int		k;
+
+	i = 0;
+	while (str[i] == ' ' || str[i] == '\t' || str[i] == '\n')
+		i++;
+	j = i;
+	while (str[i] && (str[i] != ' ' && str[i] != '\t' && str[i] != '\n'))
+	{
+		i++;
+		k = i;
+		if (str[i] == '\0')
+		{
+			write(1, &str[j], (k - j));
+			return(0);
+		}
+	}
+	while (str[i] == ' ' || str[i] == '\t' || str[i] == '\n' || str[i] == '\0')
+	{
+		if (str[i] != '\0')
+			i++;
+		if (str[i] == '\0')
+		{
+			write(1, &str[j], (k - j));
+			return(0);
+		}
+	}
+	while (str[i])
+	{
+		if (str[i] == ' ' || str[i] == '\t' || str[i] == '\n')
+		{
+			while (str[i] == ' ' || str[i] == '\t' || str[i] == '\n')
+				i++;
+			if (str[i])
+				write(1, " ", 1);
+		}
+		if (str[i] != ' ' && str[i] != '\t' && str[i] != '\n')
+			write(1, &str[i], 1);
+		i++;
+	}
+	write(1, " ", 1);
+	write(1, &str[j], (k - j));
+	return(0);
 }
